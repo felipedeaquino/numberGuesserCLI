cddf6dc71684a9b7f156e4a284f1a2111b402f50
felipedeaquino <felipe.puziol@gmail.com>
2024-08-24 03:56:40 -0300

    refactor: add options and rules to config file, rename askToPlayAgain function and continueGame variable, and refactor getFromUser

--- config.mjs
+++ config.mjs
@@ -1,23 +1,35 @@
-const texts = {
-  welcomeMessage: `Welcome to the Number Guessing Game!,
-I'm thinking of a number between 1 and 100.\n`,
-  difficultyMenu: `Please select the difficulty level:
-1. Easy (10 chances)
-2. Medium (5 chances)
-3. Hard (3 chances)\n`,
-  invalidInput: `Invalid input. Please enter a number between {min} and {max}.\n`,
-  choiceTemplate: `Enter your choice: `,
-  guessLine: `Enter your guess: `,
-  playAgain: 'Would you like to play again? (Yes/No) '
-}
-
 const levels = {
   1: { difficulty: 'Easy', attempts: 10 },
   2: { difficulty: 'Medium', attempts: 5 },
   3: { difficulty: 'Hard', attempts: 3 }
 }
 
+const options = {
+  guess: { min: 1, max: 100 },
+  difficulty: { min: 1, max: Object.keys(levels).length  }
+}
+
+const rules = {
+  isInRange: (min, max) => value => Number(value) >= min && Number(value) <= max,
+}
+
+const texts = {
+  welcomeMessage: `Welcome to the Number Guessing Game!,
+I'm thinking of a number between ${options.guess.min} and ${options.guess.max}.\n`,
+  difficultyMenu: `Please select the difficulty level:\n${
+  Object.keys(levels)
+    .map(key => `${key}. ${levels[key].difficulty} (${levels[key].attempts} chances)`)
+    .join('\n')
+  }\n`,
+  invalidInput: `Invalid input. Please enter a number between {min} and {max}.\n`,
+  choiceTemplate: `Enter your choice: `,
+  guessLine: `Enter your guess: `,
+  playAgain: 'Would you like to play again? (Yes/No) '
+}
+
 export {
   levels,
+  options,
+  rules,
   texts,
 }
--- main.mjs
+++ main.mjs
@@ -1,5 +1,5 @@
 import readline from 'node:readline';
-import { levels,texts } from './config.mjs';
+import { levels, options, rules, texts } from './config.mjs';
 
 const rl = readline.createInterface({
   input: process.stdin,
@@ -10,29 +10,34 @@ let answer;
 let difficultyLevel;
 let attempts;
 let guess;
-let guessMin = 1;
-let guessMax = 100;
-let difficultyMin = 1;
-let difficultyMax = 3;
-
-const difficultyRules = (difficultyLevel) => [1, 2, 3].includes(difficultyLevel);
-const guessRules = (guess) => Number(guess) >= guessMin && Number(guess) <= guessMax && !Number.isNaN(guess);
 
 async function initGame() {
   console.log(texts.welcomeMessage);
   console.log(texts.difficultyMenu);
-  difficultyLevel = await getFromUser({ min: difficultyMin, max: difficultyMax, value: difficultyLevel, text: texts.choiceTemplate, rules: difficultyRules, invalid: texts.invalidInput });
+  difficultyLevel = await getFromUser({
+    value: difficultyLevel,
+    type: 'difficulty',
+    text: texts.choiceTemplate,
+    rules: [rules.isInRange(options.difficulty.min, options.difficulty.max)],
+    invalid: texts.invalidInput
+  });
   console.log(`\nGreat! You have selected the ${levels[difficultyLevel].difficulty} difficulty level.\nLet's start the game!`)   
 }
 
 async function playGame() {
   attempts = 0;
-  answer = Math.floor(Math.random() * 100) + 1;
+  answer = Math.floor(Math.random() * options.guess.max) + 1;
 
   let compared = false;
 
   while (attempts < levels[difficultyLevel].attempts && !compared){
-    guess = await getFromUser({ min: guessMin, max: guessMax, value: guess, text: texts.guessLine, rules: guessRules, invalid: texts.invalidInput });
+    guess = await getFromUser({
+      value: guess,
+      type: 'guess',
+      text: texts.guessLine,
+      rules: [rules.isInRange(options.guess.min, options.guess.max)],
+      invalid: texts.invalidInput
+    });
     compared = compare(guess);
     attempts++;
   }
@@ -43,15 +48,15 @@ async function playGame() {
     console.log('You fucking lose. Loser.\n');
   }
   
-  await wannaPlayAgain();
+  await askToPlayAgain();
 }
 
-async function wannaPlayAgain() {
-  let answer;
-  do { answer = await getInput({ text: texts.playAgain, isNumber: false});
-  } while (!['YES', 'Y', 'NO', 'N'].includes(answer.toUpperCase()));
+async function askToPlayAgain() {
+  let continueGame;
+  do { continueGame = await getInput({ text: texts.playAgain, isNumber: false});
+  } while (!['YES', 'Y', 'NO', 'N'].includes(continueGame.toUpperCase()));
 
-  if (answer.toUpperCase() === 'YES' || answer.toUpperCase() === 'Y') {
+  if (continueGame.toUpperCase() === 'YES' || continueGame.toUpperCase() === 'Y') {
     await initGame();
     await playGame();
   } else {
@@ -68,14 +73,22 @@ function compare(guess) {
   return true;
 }
 
-async function getFromUser({ min = null, max = null, value, text, rules, invalid }) {
+async function getFromUser({ value, type, text, rules = [], invalid }) {
   while (true) {
     value = await getInput({ text });
-
-    if (rules(value)) {
+  
+    const isValid = rules.every(rule => rule(value));
+    if (isValid) {
       return value;
     }
-    console.log(formatInvalidInput({ template: invalid, min, max }));  
+
+    console.log(
+      formatInvalidInput({ 
+        template: invalid,
+        min: options[type].min,
+        max: options[type].max
+      }) 
+    );  
   }
 }
 
